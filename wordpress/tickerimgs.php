98a09baaa84d2652f6097abe680e7ce02dfa3e15
alfred <alfred@nurd.nu>
2016-11-11 06:08:41 +0000

    remove add media-button from wysiwygeditor

--- wordpress/tickerimgs.php
+++ wordpress/tickerimgs.php
@@ -4,4 +4,7 @@ add_image_size( 'ticker-sized', 1080, 9999 );
 
 add_filter('jpeg_quality', function($arg) { return 80; });
 
+add_action('admin_head', function() {
+	        remove_action( 'media_buttons', 'media_buttons' );
+});
 
